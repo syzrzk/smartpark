160c4ff2cf811425b48cdfe0158f0767e2ff8fac
Rizki Setiawan <rzkstwn@gmail.com>
2026-06-07 17:59:02 +0700

    Force HTTPS

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-
+use Illuminate\Support\Facades\URL;
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -18,7 +18,9 @@ class AppServiceProvider extends ServiceProvider
      * Bootstrap any application services.
      */
     public function boot(): void
-    {
-        //
+{
+    if (app()->environment('production')) {
+        URL::forceScheme('https');
     }
+}
 }
